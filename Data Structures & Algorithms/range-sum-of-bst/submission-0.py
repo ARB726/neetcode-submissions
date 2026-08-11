c8ee78057f8c6670bad5ef532b2e2568607deff3
Abdul Raziq <161387601+ARB726@users.noreply.github.com>
2026-08-10 20:33:52 -0700

    Add: range-sum-of-bst - submission-0

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-0.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.Result = 0
+
+        def helperFunction(node , low , high):
+
+            if not node:
+                return 0
+
+            if node.val >= low and node.val <= high:
+                self.Result += node.val
+
+            helperFunction(node.left,low,high)
+            helperFunction(node.right,low,high)
+
+        helperFunction(root , low , high)
+
+        return self.Result
